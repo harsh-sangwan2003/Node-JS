9e21d87627e2bf766652ea930e0a4f02a07d5200
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-13 14:43:49 +0530

    added

---
+++ Class - 3/es6Enable/one.mjs
@@ -0,0 +1,11 @@
+import { div, X, obj2 } from "./two.mjs"
+
+import obj from "./two.mjs";
+
+
+console.log(div());
+console.log(X);
+console.log(obj2);
+
+console.log(obj.add(10, 20));
+console.log(obj.mul(12, 4));
---
+++ Class - 3/es6Enable/two.mjs
@@ -0,0 +1,23 @@
+const obj = {
+
+    add(x, y) {
+
+        return x + y;
+    },
+    mul(x, y) {
+
+        return x * y;
+    }
+}
+
+export const div = () => 1000;
+
+export const obj2 = {
+
+    show() {
+
+    }
+}
+export const X = 100;
+
+export default obj;
