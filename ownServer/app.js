9929c096d30491a5ea3143e0d3934dd28712175f
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-14 20:48:49 +0530

    added

--- ownServer/app.js
+++ ownServer/app.js
@@ -1,4 +1,9 @@
 const http = require('http');
+const https = require('https');
+const urlObject = require('url');
+
+//This is depreacted -> use URLSearchParams
+const queryString = require('querystring');
 const readStaticFile = require('./utils/serveFile');
 
 let contentType = '';
@@ -9,11 +14,66 @@ function handleRequestResponse(request, response) {
     //But still no outcome because we have not mentioned when the response will end
 
     const url = request.url;
+    const method = request.method;
 
-    if (url.startsWith('/login?')) {
+    //Redirection
+    if (url == '/dashboard' && method == 'GET') {
+
+        response.write('I am in dashboard');
+        response.end();
+    }
+
+    //POST Method & routing
+    else if (url.startsWith('/login') && method == 'POST') {
+
+        let myData = '';
+
+        request.on('data', chunk => {
+
+            myData += chunk;
+        })
+
+        request.on('end', () => {
+
+            // let infoObj = queryString.parse(myData);
+            let queryParams = new URLSearchParams(myData);
+            let data = [];
+
+            for (let params of queryParams) {
+
+                data.push(params[1]);
+            }
+
+            const [userid, passwd] = data;
+
+            if (userid == 'Harsh' && passwd == 123) {
+
+                //Code for redirection
+                response.statusCode = 302;
+                response.setHeader('Location', '/dashboard');
+                // return response.end();
+            }
+
+            else
+                response.write('Invalid credentails');
+
+            response.end();
+        })
+
+    }
+
+    //GET Method
+    else if (url.startsWith('/login?') && method == 'GET') {
 
         //Login check
-        response.write("Login");
+        let userInfo = urlObject.parse(url, true);
+
+        if (userInfo.query.userid == 'Harsh' && userInfo.query.passwd == '123')
+            response.write("Login successful");
+
+        else
+            response.write("Invalid user-id or passwd");
+
         response.end();
     }
 
@@ -44,3 +104,5 @@ const serverObj = server.listen(6789, err => {
     else
         console.log("Server started!", serverObj.address().port);
 })
+
+//Certificates
