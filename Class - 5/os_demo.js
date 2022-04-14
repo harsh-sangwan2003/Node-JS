e212341ed4e99efa4483aadf639ba292569212bc
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-14 16:35:07 +0530

    added

---
+++ Class - 5/os_demo.js
@@ -0,0 +1,14 @@
+const os = require('os');
+
+console.log(os.platform());
+console.log(os.arch());
+console.log(os.type());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.release());
+
+console.log(os.cpus());
+console.log(os.networkInterfaces());
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.userInfo());
